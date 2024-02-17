ef738d44b5e4cb257a61b5d81738bb73e0b7c1fd
Amit Saini <amitssaini999@gmail.com>
2024-02-17 12:54:13 +0530

    second change

--- src/Components/Navbar/Logo.jsx
+++ src/Components/Navbar/Logo.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import logoImage from '../../assets/logoImage.png'
-export const Logo = () => {
+import styles from './logo.module.css'
+ const Logo = () => {
   return (
-    <img src={logoImage} alt='logo' width={67} height={34} top={20}  left={32}/>
+    <img src={logoImage} alt='logo' className={styles.logo}/>
   )
 }
+
+// const Logo = () => {
+//   return <img src="qtify-logo.png" alt="logo" />;
+// };
+
+ export default Logo
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import { Logo } from './Logo'
+import  Logo from './Logo'
 import Search from '../Search/Search'
 import Button from '../Button/Button'
 
-export const Navbar = () => {
+ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <Logo />
@@ -15,3 +15,32 @@ export const Navbar = () => {
     </nav>
   )
 }
+
+
+// import  Logo  from './Logo';
+// import React from 'react'
+// import styles from './Navbar.module.css'
+// import Search from '../Search/Search'
+// import Button from '../Button/Button'
+// const Navbar = () => {
+//   return (
+//     <div className="navbar bg-[#34C94B] h-[68px] flex justify-between items-center">
+//       <div className="logo ml-4 flex items-center">
+//         {" "}
+//         {/* Adjust margin as needed */}
+//         <Logo />
+//       </div>
+//       <input
+//         placeholder="Search an album of your choice"
+//         className="search-input h-10 w-[40%] rounded-xl p-5 border-2 border-black border-solid"
+//       />
+//       <button className="feedback-btn bg-[#121212] text-[#34C94B] font-Poppins rounded-xl p-3 mr-4">
+//         {" "}
+//         {/* Adjust margin */}
+//         Give Feedback
+//       </button>
+//     </div>
+//   );
+// };
+
+export default Navbar;
